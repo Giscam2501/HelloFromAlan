4339db889f50a525c22258340fff133ba9574125
Carlos Alberto León Chinchay <cleon@dosx0099.home>
2020-10-06 21:28:13 +0200

    I, Carlos, added a new line into scr/Main.java

--- src/ortiz/Main.java
+++ src/ortiz/Main.java
@@ -3,6 +3,9 @@ package ortiz;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Alan");
+
+        System.out.println("Hello Alan, my name is Carlos.");
     }
 }
